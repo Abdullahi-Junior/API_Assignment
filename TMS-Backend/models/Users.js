d6c21adc068afe78db28802b7a9cab4087577722
Abdullahi-junior <abdullahi@iut-dhaka.edu>
2025-05-10 23:17:21 +0600

    Task

--- TMS-Backend/models/Users.js
+++ TMS-Backend/models/Users.js
@@ -1,11 +1,16 @@
+// backend/models/Task.js
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const Users = new Schema({
-  author: ObjectId,
-  title: String,
-  body: String,
-  date: Date
+const Task = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    dueDate: { type: Date, required: true },
+    status: {
+        type: String,
+        enum: ['pending', 'in-progress', 'completed'],
+        default: 'pending',
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
-module.exports = mongoose.model('Users', Users);
+module.exports = mongoose.model('Task', Task);
