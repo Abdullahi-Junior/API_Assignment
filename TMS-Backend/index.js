7bede9012f6c4e4e6330087eb9fd0eb3aa611dff
Abdullahi-junior <abdullahi@iut-dhaka.edu>
2025-05-10 23:58:52 +0600

    u

--- TMS-Backend/index.js
+++ TMS-Backend/index.js
@@ -1,5 +1,12 @@
 const express = require('express')
-
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
 const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World I am Abdullahi CSE Master');
@@ -8,7 +15,8 @@ app.get('/', (req, res) => {
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
-
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 mongoose.connect('mongodb+srv://abdullahi:abdullahi@cluster0.igt9foq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => {
